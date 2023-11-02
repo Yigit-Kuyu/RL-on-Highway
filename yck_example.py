f2cb3e090f0e9edb2f036ef3651fd6ff98e5cbad
Yigit-Kuyu <yigitkuyu@gmail.com>
2023-11-02 15:13:59 +0300

    V1

---
+++ yck_example.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+import gymnasium as gym
+
+
+import highway_env
+highway_env.register_highway_envs()
+
+
+env = gym.make('highway-v0', render_mode='rgb_array')
+env.reset()
+
+for _ in range(300):
+    action = env.action_type.actions_indexes["IDLE"]
+    obs, reward, done, truncated, info = env.step(action)
+    env.render()
+
+
+plt.imshow(env.render())
+plt.show()
